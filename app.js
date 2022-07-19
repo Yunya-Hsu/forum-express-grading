72ccd280098a665505a5d5315e1214d8f60c792c
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-19 10:15:12 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { getUser } = require('./helpers/auth-helpers')
 // const getUser = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
@@ -37,6 +37,7 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
